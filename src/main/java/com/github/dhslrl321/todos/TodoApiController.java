dd279ee654545bb549a20519b79dee19cb3acbaa
dhslrl321 <dhslrl321@gmail.com>
2023-12-03 20:06:09 +0900

    commit

--- src/main/java/com/github/dhslrl321/todos/TodoApiController.java
+++ src/main/java/com/github/dhslrl321/todos/TodoApiController.java
@@ -1,6 +1,13 @@
 package com.github.dhslrl321.todos;
 
 import com.github.dhslrl321.oas.api.PetsApi;
+import com.github.dhslrl321.oas.model.CreatePetData;
+import org.springframework.http.ResponseEntity;
 
 public class TodoApiController implements PetsApi {
+
+    @Override
+    public ResponseEntity<Void> createPets(CreatePetData createPetData) {
+        return PetsApi.super.createPets(createPetData);
+    }
 }
