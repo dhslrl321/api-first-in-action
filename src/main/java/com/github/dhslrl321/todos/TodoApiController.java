17a82cdb6b78a565c17ad5d2973665a2e5cf936c
dhslrl321 <dhslrl321@gmail.com>
2023-12-03 13:33:45 +0900

    init

---
+++ src/main/java/com/github/dhslrl321/todos/TodoApiController.java
@@ -0,0 +1,6 @@
+package com.github.dhslrl321.todos;
+
+import com.github.dhslrl321.oas.api.PetsApi;
+
+public class TodoApiController implements PetsApi {
+}
---
+++ src/test/java/com/github/dhslrl321/todos/ApiFirstInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dhslrl321.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
